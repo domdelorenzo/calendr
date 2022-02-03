924f447f4fcbfcf6bc0e8f77bcf42117a8fcadce
domdelorenzo <delorenzo.breed@gmail.com>
2022-02-03 10:18:26 -0600

    Corrected model line 15 - on delete

--- calendr_django/calendr/models.py
+++ calendr_django/calendr/models.py
@@ -12,7 +12,7 @@ class Event():
   title=models.CharField(max_length=100)
   description=models.CharField(max_length=200)
   date=models.DateField()
-  user_id=models.ForeignKey(User, ondelete=modles.CASCADE, related_name='events')
+  user_id=models.ForeignKey(User, ondelete=models.CASCADE, related_name='events')
 
   def __str__(self):
     return self.title
