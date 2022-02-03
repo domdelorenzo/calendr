716b4b246870b0d7978f45fe0281ce7c915be9cd
Pablo Yocupicio <Pabloey@yahoo.com>
2022-02-03 09:35:38 -0700

    Home and calendar components created

---
+++ calendr_django/calendr-vue-app/src/router.js
@@ -0,0 +1,12 @@
+import VueRouter from 'vue-router'
+
+// import components from './components'
+
+const routes = [
+  // { path: '/', component: componentname, name: componentname}
+]
+
+export default new VueRouter({
+  routes,
+  mode: 'history'
+})
