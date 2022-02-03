3f36448a653593b6525768c030aee1dea369490f
npark27 <93529617+narrwep27@users.noreply.github.com>
2022-02-03 15:06:03 -0500

    date reformatted to match db date format; added form in Calender.vue

--- calendr_django/calendr-vue-app/src/components/Calendar.vue
+++ calendr_django/calendr-vue-app/src/components/Calendar.vue
@@ -1,8 +1,16 @@
 <template>
   <div>
-    <p>This is Calendar</p>
-    <button v-on:click="handleClick"></button>
-    <DatePicker v-model="date"> </DatePicker>
+    <h1>Calendr</h1>
+    <div class="form-container">
+      <form>
+        <input type="text" placeholder="event name" />
+        <input type='text' placeholder='description' />
+        <button type="submit">Create Event</button>
+      </form>
+      <div class="calendar-div" v-on:click='handleClick'>
+        <DatePicker is-expanded v-model="date" />
+      </div>
+    </div>
   </div>
 </template>
 
@@ -21,10 +29,28 @@ export default {
   },
   methods: {
     handleClick() {
-      console.log(this.date);
+      console.log(this.date.toISOString().slice(0,10))
     },
   },
 };
 </script>
 
-<style></style>
+<style>
+  .form-container {
+    display: flex;
+    justify-content: space-around;
+  }
+  form, .calendar-div {
+    width: 40%;
+  }
+  form {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 2em;
+    padding: 2em
+  }
+  input, button {
+    width: 60%;
+  }
+</style>
