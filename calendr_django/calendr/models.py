c24d3a559ac8be91b8baa35c8e689de60b2ea0d7
domdelorenzo <delorenzo.breed@gmail.com>
2022-02-03 11:09:15 -0600

    Added rest_framework to settings.py

--- calendr_django/calendr/models.py
+++ calendr_django/calendr/models.py
@@ -2,17 +2,17 @@ from django.db import models
 
 # Create your models here.
 
-class User():
+class User(models.Model):
   username=models.CharField(max_length=50)
   def __str__(self):
     return self.username
 
 
-class Event():
+class Event(models.Model):
   title=models.CharField(max_length=100)
   description=models.CharField(max_length=200)
   date=models.DateField()
-  user_id=models.ForeignKey(User, ondelete=models.CASCADE, related_name='events')
+  user=models.ForeignKey(User, on_delete=models.CASCADE, related_name='events')
 
   def __str__(self):
     return self.title
--- calendr_django/calendr_django/urls.py
+++ calendr_django/calendr_django/urls.py
@@ -20,5 +20,5 @@ from django.conf.urls import include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('calendr.urls')),
-    path('api-auth', include('rest_frameworks.urls', namespace='rest_framework'))
+    path('api-auth', include('rest_framework.urls', namespace='rest_framework'))
 ]
