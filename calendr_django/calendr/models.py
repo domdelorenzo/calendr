a0a42b4e65aaad6b4532bc2df6a681ffd58d8a92
domdelorenzo <delorenzo.breed@gmail.com>
2022-02-03 10:07:24 -0600

    Initialized user and event models

--- calendr_django/calendr/models.py
+++ calendr_django/calendr/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class User():
+  username=models.CharField(max_length=50)
+  def __str__(self):
+    return self.username
+
+
+class Event():
+  title=models.CharField(max_length=100)
+  description=models.CharField(max_length=200)
+  date=models.DateField()
+  user_id=models.ForeignKey(User, ondelete=modles.CASCADE, related_name='events')
+
+  def __str__(self):
+    return self.title
