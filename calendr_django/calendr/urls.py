ba1e34c840905fb71ce4e355539f37719e299746
Jerome Wy <jeromedaniel.wy@gmail.com>
2022-02-03 09:17:35 -0800

    Ran the makemigrations and migrate commands

--- calendr_django/calendr/urls.py
+++ calendr_django/calendr/urls.py
@@ -3,8 +3,8 @@ from . import views
 from rest_framework.routers import DefaultRouter
 
 urlpatterns = [
-  path('users/', views.UserList.as_view(), name='user_list'),
-  path('users/<int:pk>', views.UserDetail.as_view(), name='user_detail'),
-  path('events/', views.EventList.as_view(), name='event_list'),
-  path('events/<int:pk>', views.EventDetail.as_view(), name='event_detail'),
+    path('users/', views.UserList.as_view(), name='user_list'),
+    path('users/<int:pk>', views.UserDetail.as_view(), name='user_detail'),
+    path('events/', views.EventList.as_view(), name='event_list'),
+    path('events/<int:pk>', views.EventDetail.as_view(), name='event_detail'),
 ]
