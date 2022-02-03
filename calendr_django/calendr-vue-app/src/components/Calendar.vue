8b83104e61e9e027961f40c0c1b5af0c5e16a35c
Pablo Yocupicio <Pabloey@yahoo.com>
2022-02-03 12:28:51 -0700

    Date being returned from calendar Component

--- calendr_django/calendr-vue-app/src/components/Calendar.vue
+++ calendr_django/calendr-vue-app/src/components/Calendar.vue
@@ -1,15 +1,30 @@
 <template>
-<div>
-  <p>This is Calendar</p>
-</div>
+  <div>
+    <p>This is Calendar</p>
+    <button v-on:click="handleClick"></button>
+    <DatePicker v-model="date"> </DatePicker>
+  </div>
 </template>
 
 <script>
+import DatePicker from "v-calendar/lib/components/date-picker.umd";
+
 export default {
-  name: 'Calendar',
-}
+  name: "Calendar",
+  components: {
+    DatePicker,
+  },
+  data() {
+    return {
+      date: new Date(),
+    };
+  },
+  methods: {
+    handleClick() {
+      console.log(this.date);
+    },
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
