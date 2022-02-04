c6b80636bb08b92112335bb7816c49cb87834e00
npark27 <93529617+narrwep27@users.noreply.github.com>
2022-02-03 19:29:30 -0500

    added events calendar to show all events of a user

--- calendr_django/calendr-vue-app/src/components/Calendar.vue
+++ calendr_django/calendr-vue-app/src/components/Calendar.vue
@@ -1,6 +1,19 @@
 <template>
   <div>
     <h1>Calendr</h1>
+    <a href="">Logout</a>
+    <div>
+      <VCalendar is-expanded :attributes='attributes'>
+        <template v-slot:day-content='{ day, attributes }'>
+          <span>{{ day.day }}</span>
+          <div>
+            <p :key="attr.key" v-for="attr in attributes">
+              {{ attr.customData.title }}
+            </p>
+          </div>
+        </template>
+      </VCalendar>
+    </div>
     <div class="form-container">
       <form>
         <input type="text" placeholder="event name" />
@@ -16,15 +29,50 @@
 
 <script>
 import DatePicker from "v-calendar/lib/components/date-picker.umd";
+import VCalendar from 'v-calendar/lib/components/calendar.umd';
 
 export default {
   name: "Calendar",
   components: {
     DatePicker,
+    VCalendar
   },
   data() {
     return {
       date: new Date(),
+      attributes: [
+        {
+          key: 1,
+          customData: {
+            title: 'test1'
+          },
+          dates: new Date(2022, 1, 1)
+        },
+        {
+          key: 2,
+          customData: {
+            title: 'test2'
+          },
+          dates: new Date(2022, 1, 5)
+        },
+        {
+          key: 3,
+          customData: {
+            title: 'test3'
+          },
+          dates: new Date(2022, 1, 20)
+        },
+        {
+          key: 4,
+          customData: { title: 'test4' },
+          dates: new Date(2022, 1, 1)
+        },
+        {
+          key: 5,
+          customData: { title: 'test5' },
+          dates: new Date(2022, 1, 1)
+        }
+      ]
     };
   },
   methods: {
@@ -36,6 +84,11 @@ export default {
 </script>
 
 <style>
+  a {
+    position: absolute;
+    top: 10%;
+    right: 10%;
+  }
   .form-container {
     display: flex;
     justify-content: space-around;
